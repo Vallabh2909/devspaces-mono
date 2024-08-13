53c78bff307c12f943c2ff9ba8a06d3f386bd555
Vallabh2909 <vallabhwasule913@gmail.com>
2024-08-13 20:00:44 +0530

    Added registration-service

--- codespaces/backend/nodejs-services/registration-service/src/models/user.model.js
+++ codespaces/backend/nodejs-services/registration-service/src/models/user.model.js
@@ -42,9 +42,6 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
-    refreshToken: {
-      type: String,
-    },
   },
   { timestamps: true },
 );
@@ -81,32 +78,31 @@ userSchema.post("save", async function () {
 
 export const User = mongoose.model("User", userSchema);
 
-// userSchema.methods.isPasswordCorrect = async function (password) {
-//   return await bcrypt.compare(password, this.password);
-// };
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-// userSchema.methods.generateAccessToken = function () {
-//   return jwt.sign(
-//     {
-//       _id: this._id,
-//       email: this.email,
-//       username: this.username,
-//       fullName: this.fullName,
-//     },
-//     process.env.ACCESS_TOKEN_SECRET,
-//     {
-//       expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
-//     },
-//   );
-// };
-// userSchema.methods.generateRefreshToken = function () {
-//   return jwt.sign(
-//     {
-//       _id: this._id,
-//     },
-//     process.env.REFRESH_TOKEN_SECRET,
-//     {
-//       expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
-//     },
-//   );
-// };
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
+  );
+};
--- codespaces/backend/nodejs-services/registration-service/src/services/user.services.js
+++ codespaces/backend/nodejs-services/registration-service/src/services/user.services.js
@@ -1,6 +1,5 @@
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
 import uploadOnCloudinary from "../utils/cloudinary.js";
 // class User{
 //     user={};
